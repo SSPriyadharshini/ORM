21ff838026bbe875547d10abeb88042eb52ef377
sspriyadharshini <priyapriyadharshinii2022@gmail.com>
2023-10-18 11:22:59 +0530

    Success

---
+++ priya/myapp/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.contrib import admin
+class footballplayer (models.Model):
+    name=models.CharField(max_length=20)
+    player_id=models.CharField(max_length=100)
+    weight=models.IntegerField()
+    age=models.IntegerField()
+    members=models.CharField(max_length=20)
+
+class footballplayerAdmin(admin.ModelAdmin):
+    list_display=('name','player_id','weight','age','members')
